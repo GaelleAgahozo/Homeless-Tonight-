370aef5cb6b890acf54f92f17f9198cca2a23021
Ryan Fuller <fullerra@hendrix.edu>
2022-10-25 20:36:18 -0500

    Created GUI for Homeless Home Page
    Created the GUI for when the homeless person using the app signs in and is greeted with options for services and resources.

---
+++ homeless_tonight/lib/homeless.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Homeless Services Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              ElevatedButton(
+                  onPressed: null,
+                  child: Text(
+                      "\n I am homeless in Conway \n and I do not have a place \n to sleep tonight \n",
+                      style: TextStyle(fontSize: 24, height: 1.1))),
+              Padding(
+                  padding: EdgeInsets.only(top: 50),
+                  child: ElevatedButton(
+                      onPressed: null,
+                      child: Text(
+                          "\n I am a victim of Domestic \n Violence/Sex Trafficking \n",
+                          style: TextStyle(fontSize: 24, height: 1.1)))),
+              Padding(
+                  padding: EdgeInsets.only(top: 50),
+                  child: ElevatedButton(
+                      onPressed: null,
+                      child: Text("\n Resources \n",
+                          style: TextStyle(fontSize: 24, height: 1.1))))
+            ],
+          ),
+        ));
+  }
+}
