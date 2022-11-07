e3ac33187d5747f85d37a9836a32be235d17c10f
Isaac Khounborine <KhounborineII@hendrix.edu>
2022-11-07 12:34:49 -0600

    Homeless Page now the first screen
    Initial Screen no longer exists as it was not an intended piece of our project. 
    Text Button on Homeless Page now exists to replace the old Service Provider Button on the Initial Screen

--- homeless_tonight/lib/homeless.dart
+++ homeless_tonight/lib/homeless.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:homeless_tonight/pageTemplate.dart';
 import 'package:homeless_tonight/resources_screen.dart';
 import 'package:homeless_tonight/domesticviolence.dart';
 import 'package:homeless_tonight/need_shelter.dart';
@@ -23,62 +24,59 @@ import 'package:homeless_tonight/need_shelter.dart';
 //   }
 // }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomelessMainScreen extends StatefulWidget {
+  const HomelessMainScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomelessMainScreen> createState() => _HomelessMainScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomelessMainScreenState extends State<HomelessMainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
+    return HomelessTonightPage(
+        child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+              onPressed: () => {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: ((context) => Shelter())))
+                  },
+              child: Text(
+                  "\n I am homeless in Conway \n and I do not have a place \n to sleep tonight \n",
+                  style: TextStyle(fontSize: 24, height: 1.1))),
+          Padding(
+              padding: EdgeInsets.only(top: 50),
+              child: ElevatedButton(
                   onPressed: () => {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: ((context) => Shelter())))
+                                builder: ((context) =>
+                                    DomesticViolenceScreen())))
                       },
                   child: Text(
-                      "\n I am homeless in Conway \n and I do not have a place \n to sleep tonight \n",
-                      style: TextStyle(fontSize: 24, height: 1.1))),
-              Padding(
-                  padding: EdgeInsets.only(top: 50),
-                  child: ElevatedButton(
-                      onPressed: () => {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: ((context) =>
-                                        DomesticViolenceScreen())))
-                          },
-                      child: Text(
-                          "\n I am a victim of Domestic \n Violence/Sex Trafficking \n",
-                          style: TextStyle(fontSize: 24, height: 1.1)))),
-              Padding(
-                  padding: EdgeInsets.only(top: 50),
-                  child: ElevatedButton(
-                      onPressed: () => {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: ((context) => ResourcesScreen())))
-                          },
-                      child: Text("\n Resources \n",
-                          style: TextStyle(fontSize: 24, height: 1.1))))
-            ],
-          ),
-        ));
+                      "\n I am a victim of Domestic \n Violence/Sex Trafficking \n",
+                      style: TextStyle(fontSize: 24, height: 1.1)))),
+          Padding(
+              padding: EdgeInsets.only(top: 50),
+              child: ElevatedButton(
+                  onPressed: () => {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => ResourcesScreen())))
+                      },
+                  child: Text("\n Resources \n",
+                      style: TextStyle(fontSize: 24, height: 1.1)))),
+          Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: TextButton(
+                  onPressed: () {}, child: Text("I am a Service Provider")))
+        ],
+      ),
+    ));
   }
 }
