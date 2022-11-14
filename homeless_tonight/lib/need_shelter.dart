0555a5eef989200a15acc5e9afdd080da3c29419
Isaac Khounborine <KhounborineII@hendrix.edu>
2022-11-14 10:49:26 -0600

    Need Shelter Refitted to include Alert Dialog Button

--- homeless_tonight/lib/need_shelter.dart
+++ homeless_tonight/lib/need_shelter.dart
@@ -10,98 +10,114 @@ class Shelter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return HomelessTonightPage(
-        child: Center(
-            child: Column(children: [
-      const Spacer(),
-      TextButton(
-          onPressed: () {},
-          style: TextButton.styleFrom(
-              backgroundColor: Colors.teal[200],
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18))),
-          child: const Text(
-            "I am homeless in \n Conway and don't \n have a place to \n sleep tonight ",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 27,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+        child: Column(children: [
+      const Align(
+        alignment: Alignment.center,
+      ),
+      Padding(
+          padding: const EdgeInsets.only(top: 60, bottom: 30),
+          child: Card(
+              color: Theme.of(context).colorScheme.surface,
+              child: const Text(
+                "\nI am a homeless\n in Conway and do not have \n a place to sleep tonight\n",
+                style: TextStyle(fontSize: 22),
+                textAlign: TextAlign.center,
+              ))),
+      Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: SizedBox(
+            width: 300,
+            height: 100,
+            child: ElevatedButton(
+                onPressed: () {
+                  _displayTextInputDialog(context);
+                },
+                child: const Text(
+                  "Connect with a local \n homeless service \n provider",
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                  textAlign: TextAlign.center,
+                )),
           )),
-      const Padding(
-          padding: EdgeInsets.only(top: 30, bottom: 70),
-          child: Text("Connect with a local \n homeless service \n provider",
-              textAlign: TextAlign.center, style: TextStyle(fontSize: 22))),
-      const SizedBox(
-        width: 300,
-        height: 80,
-        child: TextField(
-          decoration: InputDecoration(hintText: "1. Enter Your Name"),
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold), //Connect User Inputs to Controller
-          controller: null,
-        ),
-      ),
-      // const Padding(
-      //     padding: EdgeInsets.only(top: 5, bottom: 50),
-      //     child: Text(" 1. Enter Your Name",
-      //         style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
-      // const Padding(
-      //     padding: EdgeInsets.only(top: 4, bottom: 50),
-      //     child: Text(
-      //       " 2. Enter Current Location",
-      //       style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      //       textAlign: TextAlign.center,
-      //     )),
-      const SizedBox(
-        width: 300,
-        height: 80,
-        child: TextField(
-          decoration: InputDecoration(hintText: "2. Enter Current Location"),
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold), //Connect User Inputs to Controller
-          controller: null,
-        ),
-      ),
-      // const Padding(
-      //     padding: EdgeInsets.only(top: 20, bottom: 60),
-      //     child: Text(
-      //       " 3. What Type of Shelter Do You Need?",
-      //       style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      //       textAlign: TextAlign.center,
-      //     )),
-      const SizedBox(
-        width: 300,
-        height: 80,
-        child: TextField(
-          decoration: InputDecoration(
-              hintText: " 3. What Type of Shelter Do You Need?"),
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold), //Connect User Inputs to Controller
-          controller: null,
-        ),
-      ),
-      TextButton(
-          onPressed: () {},
-          style: TextButton.styleFrom(
-              backgroundColor: Colors.teal[200],
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8))),
-          child: const Text(
-            "Send ",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          )),
-    ])));
+    ]));
   }
+}
+
+final TextEditingController nameController = TextEditingController();
+final TextEditingController locationController = TextEditingController();
+final TextEditingController shelterController = TextEditingController();
+final ButtonStyle yesStyle = ElevatedButton.styleFrom(
+    textStyle: const TextStyle(fontSize: 20), backgroundColor: Colors.green);
+final ButtonStyle noStyle = ElevatedButton.styleFrom(
+    textStyle: const TextStyle(fontSize: 20), backgroundColor: Colors.red);
+String nameText = '';
+String locationText = '';
+String shelterText = '';
+
+Future<void> _displayTextInputDialog(BuildContext context) async {
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('I am Homeless and Need Shelter'),
+          content: Column(mainAxisSize: MainAxisSize.min, children: [
+            TextField(
+              onChanged: (value) {
+                nameText = value;
+              },
+              controller: nameController,
+              decoration: const InputDecoration(hintText: "Enter Your Name"),
+            ),
+            TextField(
+              onChanged: (value) {
+                locationText = value;
+              },
+              controller: locationController,
+              decoration: const InputDecoration(
+                  hintText: "Enter Your General Location"),
+            ),
+            TextField(
+              onChanged: (value) {
+                locationText = value;
+              },
+              controller: shelterController,
+              decoration: const InputDecoration(
+                  hintText: "Enter What Kind of Shelter You Need"),
+            ),
+          ]),
+          actions: <Widget>[
+            ElevatedButton(
+              key: const Key("OKButton"),
+              style: yesStyle,
+              child: const Text('OK'),
+              onPressed: () {
+                _handleNewItem(nameText);
+                _handleNewItem(locationText);
+                _handleNewItem(shelterText);
+                Navigator.pop(context);
+              },
+            ),
+
+            // https://stackoverflow.com/questions/52468987/how-to-turn-disabled-button-into-enabled-button-depending-on-conditions
+            ValueListenableBuilder<TextEditingValue>(
+              valueListenable: shelterController,
+              builder: (context, value, child) {
+                return ElevatedButton(
+                  key: const Key("CancelButton"),
+                  style: noStyle,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancel'),
+                );
+              },
+            ),
+          ],
+        );
+      });
+}
+
+void _handleNewItem(String itemText) {
+  nameController.clear();
+  locationController.clear();
+  shelterController.clear();
 }
