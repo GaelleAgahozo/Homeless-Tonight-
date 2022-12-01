1acb888227b2308592c5ab17e6a8578a531e9ef6
Ketsia <dusengeketty1@gmail.com>
2022-12-01 17:31:48 -0600

    Appbar

---
+++ homeless_tonight/lib/messageui.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(70),
+            child: AppBar(
+              iconTheme: const IconThemeData(color: Color(0xff73bfb8)),
+              centerTitle: true,
+              title: Align(
+                alignment: Alignment.center,
+                child: Image.asset('assets/images/logoTSC.png',
+                    fit: BoxFit.fitWidth, height: 60, width: 130),
+              ),
+              backgroundColor: Colors.white,
+            )));
+  }
+}
